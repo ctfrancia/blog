e9d6d669058e89493475f3210e2e1fb51e5bfbe1
Christian Francia <christian.francia@tui.com>
2024-11-14 14:17:29 +0100

    feat: render html and styling

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/yuin/goldmark"
 	highlighting "github.com/yuin/goldmark-highlighting/v2"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -15,7 +16,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	fr := FileReader{}
-	mux.HandleFunc("GET /posts/{slug}", PostHandler(fr))
+	postTemplate := template.Must(template.ParseFiles("cmd/templates/post.gohtml"))
+	mux.HandleFunc("GET /posts/{slug}", PostHandler(fr, postTemplate))
 
 	err := http.ListenAndServe(":3030", mux)
 	if err != nil {
@@ -26,6 +28,11 @@ func main() {
 
 type SlugReader interface {
 	Read(slug string) (string, error)
+}
+type PostData struct {
+	Title   string
+	Content template.HTML
+	Author  string
 }
 
 type FileReader struct{}
@@ -45,7 +52,7 @@ func (fr FileReader) Read(slug string) (string, error) {
 	return string(b), nil
 }
 
-func PostHandler(sl SlugReader) http.HandlerFunc {
+func PostHandler(sl SlugReader, tpl *template.Template) http.HandlerFunc {
 	mdRenderer := goldmark.New(
 		goldmark.WithExtensions(
 			highlighting.NewHighlighting(
@@ -53,24 +60,30 @@ func PostHandler(sl SlugReader) http.HandlerFunc {
 			),
 		),
 	)
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		slug := r.PathValue("slug")
-		file := fmt.Sprintf("internal/posts/%s.md", slug)
-		content, err := sl.Read(file)
+		pp := fmt.Sprintf("internal/posts/%s.md", slug)
+		postMarkdown, err := sl.Read(pp)
 		if err != nil {
-			fmt.Println(err)
-			// TODO: log error according to the context
+			log.Println(err)
 			http.Error(w, "Post not found", http.StatusNotFound)
 			return
 		}
 
 		var buf bytes.Buffer
-		err = mdRenderer.Convert([]byte(content), &buf)
+		err = mdRenderer.Convert([]byte(postMarkdown), &buf)
 		if err != nil {
-			panic(err)
+			http.Error(w, "Error converting markdown", http.StatusInternalServerError)
 		}
-		io.Copy(w, &buf)
-		fmt.Fprint(w, buf.String())
+
+		err = tpl.Execute(w, PostData{
+			Content: template.HTML(buf.String()),
+			Author:  "Jon Calhoun",
+			Title:   "My Blog",
+		})
+		if err != nil {
+			http.Error(w, "Error executing template", http.StatusInternalServerError)
+			return
+		}
 	}
 }
